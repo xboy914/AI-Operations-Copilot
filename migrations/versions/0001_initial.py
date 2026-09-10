3178afa0d2cb8a68a8f5e6cf07b2eae7835f3425
Sajjad Safari <sajjadsafari914@gmail.com>
2026-09-10 13:06:09 +0330

    style: satisfy backend quality checks

--- migrations/versions/0001_initial.py
+++ migrations/versions/0001_initial.py
@@ -1,7 +1,7 @@
 """initial identity, workflow, approval, and audit schema"""
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 revision = "0001"
